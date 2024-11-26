d7ba3324e472ddf31c68e9ccf1507c95437a2a24
ThePowerfulPierog <mikecphillips95@gmail.com>
2024-11-25 21:07:17 -0500

    corrected streamlit file

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.header('Tossing a Coin')
 
-number_of_trials = st.slider("Number of trials?', 1, 1000, 10)
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
 start_button = st.button('Run')
 
 if start_button:
